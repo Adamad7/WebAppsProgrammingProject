6f66d4195d1e9dee1a1a4996235839ae0e16bd7c
Adam Mazur <adam.mazur951@gmail.com>
2024-01-05 00:01:02 +0100

    Dodanie możliwości logowania

--- sklep-wedkarski/resources/views/layouts/navbar.blade.php
+++ sklep-wedkarski/resources/views/layouts/navbar.blade.php
@@ -1,19 +1,32 @@
 <nav>
-        <a href="{{ url('/') }}">Strona główna</a>
-        <a href="{{ url('/shop') }}">Sklep</a>
-        <a href="{{ url('/blog') }}">Blog</a>
-        <div class="dropdown">
-            <button class="dropbtn">Niezbędnik
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href="{{ url('/fishing_grounds') }}">Łowiska</a>
-                <a href="{{ url('/calendar') }}">Kalendarz</a>
-            </div>
+    <a href="{{ url('/') }}">Strona główna</a>
+    <a href="{{ url('/shop') }}">Sklep</a>
+    <a href="{{ url('/blog') }}">Blog</a>
+    <div class="dropdown">
+        <button class="dropbtn">Niezbędnik
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="{{ url('/fishing_grounds') }}">Łowiska</a>
+            <a href="{{ url('/calendar') }}">Kalendarz</a>
         </div>
-        <a href="{{ url('/newsletter') }}">Newsletter</a>
-        <a href="{{ url('/sources') }}">Źródła</a>
-        <a href="{{ url('/cart') }}">
-            <div id="cart_value"><i class="fas fa-shopping-cart"></i> (0zł)</div>
-        </a>
-    </nav>
+    </div>
+    <a href="{{ url('/newsletter') }}">Newsletter</a>
+    <a href="{{ url('/sources') }}">Źródła</a>
+    <a href="{{ url('/cart') }}">
+        <div id="cart_value"><i class="fas fa-shopping-cart"></i> (0zł)</div>
+    </a>
+    @if (Route::has('login'))
+
+    @auth
+    <a href="{{ url('/dashboard') }}">{{  Auth::user()->name  }}</a>
+    @else
+    <a href="{{ route('login') }}">Zaloguj się</a>
+
+    @if (Route::has('register'))
+    <a href="{{ route('register') }}">Zarejestruj się</a>
+    @endif
+    @endauth
+
+    @endif
+</nav>
